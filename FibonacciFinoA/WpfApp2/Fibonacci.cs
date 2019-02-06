7375972843d78e90e6f643989710152849d15b78
Emanuele Pagotto <pago1998@gmail.com>
2019-02-06 14:27:22 +0100

    FibonacciFinoA
    per colloquio

---
+++ FibonacciFinoA/WpfApp2/Fibonacci.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciFinoA
+{
+    class Fibonacci
+    {
+        public List <int> list { get; private set; }
+        public int next { get; private set; }
+
+        public Fibonacci()
+        {
+            reset();
+        }
+
+        public void gen()
+        {
+            list.Add(next);
+            next = (list.Count == 1) ? 1 : list[list.Count - 1] + list[list.Count - 2];
+        }
+
+        public void reset()
+        {
+            list = new List<int>();
+            next = 0;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(int i in list)
+            {
+                sb.Append(i + "\n");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ FibonacciFinoA/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FibonacciFinoA
+{
+    public partial class MainWindow : Window
+    {
+        private Fibonacci fib;
+        private ListView listFib;
+
+        public MainWindow()
+        {
+            fib = new Fibonacci();
+            InitializeComponent();
+            listFib = (ListView) lst_fib;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            fib.reset();
+            String text = txt_maxN.Text;
+            int maxN = Int32.Parse(text);
+
+            while(fib.next < maxN)
+            {
+                fib.gen();
+            }
+
+            lst_fib.ItemsSource = fib.list;
+        }
+    }
+}
